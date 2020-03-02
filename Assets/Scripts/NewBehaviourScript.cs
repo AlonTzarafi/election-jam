488444ad88f8fd24b95b54ad5afe771e44715ad6
Chedvah-b <58727484+Chedvah-b@users.noreply.github.com>
2020-03-02 22:15:25 +0200

    All done

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public Image red;
+    float max_percentage = 100f;
+    float current_percentage = 50f;
+
     // Start is called before the first frame update
     void Start()
     {
